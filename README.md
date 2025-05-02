957e6840fe2a5501a2648f6dd598afbcf8970fd1
DenniKs <dennikz@yandex.ru>
2025-05-02 22:23:10 +0300

    feat: готов проект спринта 5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+Cсылка на репозитории: https://github.com/DenniKs/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const listCard = document.querySelector('.places__list');
+
+function createCard(Card, deleteCard) {
+    const templateList = document.querySelector('#card-template').content;
+    const templateCard = templateList.querySelector('.card').cloneNode(true);
+    const image = templateCard.querySelector('.card__image');
+    image.src = Card.link;
+    image.alt = Card.name;
+    templateCard.querySelector('.card__title').textContent = Card.name;
+    templateCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return templateCard;
+};
+
+function deleteCard(evt) {
+    const eventClick = evt.target;
+    const buttonList = eventClick.closest('.card');
+    buttonList.remove();
+};
+
+initialCards.forEach(function (item) {
+    const newCard = createCard(item, deleteCard);
+    listCard.append(newCard);
+});
